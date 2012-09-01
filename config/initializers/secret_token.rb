a1017f96af13b6923882fb232490e99a93051068
Luc Boissaye <luc@boissaye.fr>
2012-09-01 14:21:45 +0200

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookLikes::Application.config.secret_token = '1b56844d9269146b90f980fcdd63a325da676db3dc7b8ea98058face68701290ef8a47a7e2181a8904971b6f9f2472ee402933d739d0f27a9dcf32d7f3986625'
